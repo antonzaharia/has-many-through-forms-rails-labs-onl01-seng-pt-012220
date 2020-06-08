ea5a512feb3aa6f3f6aef1275aacff9f362b060c
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-08 19:34:29 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,14 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories, reject_if: :all_blank
 
-  def comments_attributes=(comment_attributes)
-      comment_attributes.values.each do |comment_attribute|
-        comment = Comment.find_or_create_by(comment_attribute)
-        self.comment_attributes.build(comment: comment)
-      end
-    end
-
+  def categories_attributes=(categories_attributes)
+  	categories_attributes.each do |i, category_attr|
+	  	if category_attr[:name].present?
+	  		category = Category.find_or_create_by(name: category_attr[:name])
+	  		if !self.categories.include?(category)
+	  			self.post_categories.build(:category => category)
+	  		end
+	  	end
+  	end
+  end
 end
