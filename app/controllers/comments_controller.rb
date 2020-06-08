7a565840e1a5ccd914aac1e36a2de08272327d1d
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-08 19:04:03 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
 
+  def show
+    @category = Category.find(params[:id])
+  end
+
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
